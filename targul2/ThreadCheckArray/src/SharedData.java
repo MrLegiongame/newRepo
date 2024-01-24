09d44aad66f3ebe48f32701cd99d4b6972929fac
Adir111 <adir987321@gmail.com>
2024-01-24 18:49:02 +0200

    SharedData docs

--- targul2/ThreadCheckArray/src/SharedData.java
+++ targul2/ThreadCheckArray/src/SharedData.java
@@ -7,37 +7,60 @@ public class SharedData
 	private boolean flag;
 	private final int b;
 	
-	public SharedData(ArrayList<Integer> array, int b) {
-		
+	/**
+	 * @param array ArrayList with numbers of type Integers.
+	 * @param b Indicates the required sum of two numbers in array.
+	 * <br>Initializing the SharedData object with the given array and b values.
+	 */
+	public SharedData(ArrayList<Integer> array, int b) 
+	{	
 		this.array = array;
 		this.b = b;
 	}
 
+	/**
+	 * @return Returns the winArray of the class.
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
 
+	/**
+	 * @param winArray Updating the class winArray to the given winArray.
+	 */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
+	/**
+	 * @return Returns array of the class.
+	 */
 	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
 
+	/**
+	 * @return Returns b of the class.
+	 */
 	public int getB() 
 	{
 		return b;
 	}
 
+	/**
+	 * @return Returns the class flag value.
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
 
+	/**
+	 * @param flag Updating the class flag to the given flag.
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
